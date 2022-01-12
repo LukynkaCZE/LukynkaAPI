143a5e30a1a27536d52dc96edf6e66b2619e52fa
LukynkaCZE <48604271+LukynkaCZE@users.noreply.github.com>
2022-01-12 17:59:59 +0100

    Added random location function

---
+++ src/main/java/cz/lukynka/api/LocationUtils.java
@@ -0,0 +1,26 @@
+package cz.lukynka.api;
+
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public abstract class LocationUtils {
+
+    public static Location randomLocationInRadius(Location center, int min, int max, boolean sphere) {
+
+        int rx = Maths.getRandomNumber(min, max);
+        int rz = Maths.getRandomNumber(min, max);
+        int ry = Maths.getRandomNumber(min, max);
+
+        Location loc = null;
+
+        if(sphere) {
+            loc = center.clone().add(new Vector(rx, ry, rz));
+        } else {
+            loc = center.clone().add(new Vector(rx, 0, rz));
+        }
+        
+        return loc;
+    }
+
+
+}
